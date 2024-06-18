7c30324f917926d4d43560702214740c0b8af27c
tam-bussaba <102574772+tam-bussaba@users.noreply.github.com>
2024-06-18 17:08:14 +0700

    up

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -2,70 +2,101 @@
 
 import React from 'react';
 
-const Card = ({ title, description,imageUrl,price }) => {
+const Card = ({ title, description, imageUrl, price }) => {
   return (
     <div className="card card-transform">
       {/* {imageUrl && <img src={imageUrl} className="card-img-top" alt={title} />} */}
       <div className="card-body">
 
-        
-<div className="row">
-<div className="col-2 mb-3">
-        {imageUrl && <img src={imageUrl} className="card-img-top" alt={title} />}
-</div>
-<div className="col-6 mb-3">
-    <div className="mt-2">
-    <h4>{title}</h4>
-    <span>{description}</span>
-    </div>
-</div>
-<div className="col-4 mb-3" align="right">
-    <div className="mt-2">
-        <h4>฿ {price}</h4>
-        <span>ผ่อน 8 เดือน</span>
-    </div>
-</div>
-</div>
+
+        <div className="row">
+          <div className="col-2 mb-3">
+            {imageUrl && <img src={imageUrl} className="card-img-top" alt={title} />}
+          </div>
+          <div className="col-6 mb-3">
+            <div className="mt-2">
+              <h4>{title}</h4>
+              <span>{description}</span>
+            </div>
+          </div>
+          <div className="col-4 mb-5" align="right">
+            <div className="mt-2">
+              <h4>฿ {price}</h4>
+              <span>ผ่อน 8 เดือน</span>
+            </div>
+          </div>
+        </div>
 
 
         <div className="row" align="center">
 
-            <div className="col-4 mb-3">
+          <div className="col-4 mb-3">
             <div className="card">
-            <div className="card-body">
-            <h6 className="card-title">฿ 100,000</h6>
-            <p className="card-text">ทุนประกัน  <br/> <br/></p>
+              <div className="card-body">
+                <h6 className="card-title">฿ 100,000</h6>
+                <p className="card-text">ทุนประกัน  <br /> <br /></p>
+              </div>
             </div>
-            </div>
-            </div>
-            
-            <div className="col-4 mb-3">
+          </div>
+
+          <div className="col-4 mb-3">
             <div className="card">
-            <div className="card-body">
-            <h6 className="card-title">ซ่อมอู่</h6>
-            <p className="card-text">ประเภทการซ่อม <br/> <br/></p>
+              <div className="card-body">
+                <h6 className="card-title">ซ่อมอู่</h6>
+                <p className="card-text">ประเภทการซ่อม <br /> <br /></p>
+              </div>
             </div>
-            </div>
-            </div>
-            
-            <div className="col-4 mb-3">
+          </div>
+
+          <div className="col-4 mb-3">
             <div className="card">
-            <div className="card-body">
-            <h6 className="card-title">฿ 2,500,000</h6>
-            <p className="card-text">ทรัพย์สินบุคคล ภายนอก</p>
+              <div className="card-body">
+                <h6 className="card-title">฿ 2,500,000</h6>
+                <p className="card-text">ทรัพย์สินบุคคล ภายนอก <br></br></p>
+              </div>
             </div>
-            </div>
-            </div>
+          </div>
 
         </div>
 
-        <div className="alert alert-secondary" role="alert">
-        ผ่อนเงินสด 0% นาน 12 เดือน ไม่ง้อบัตรเครดิต
-            </div>
 
-            <div className="d-grid gap-2">
-            <button className="btn btn-primary" type="button">สนใจแผนประกัน</button>
+        <div className="alert alert-secondary" role="alert">
+          ผ่อนเงินสด 0% นาน 12 เดือน ไม่ง้อบัตรเครดิต
+        </div>
+
+        <div className="row" align="right">
+        <div className="col-3 mb-3" role="alert">
+          เปรียบเทียบ
+        </div>
+
+        <div className="col-3 mb-3" role="alert">
+          ดูรายละเอียด
+        </div>
+        </div>
+
+        {/* <div className="col-15 mb-4" align="right" >
+          <div className="row"  >
+            <div className="mt-2">
+              <span>เปรียบเทียบ</span>
             </div>
+          </div>
+        </div>
+
+        <div className="col-15 mb-4" align="right" >
+          <div className="row"  >
+            <div className="mt-2">
+              <span>รายละเอียด</span>
+            </div>
+          </div>
+        </div> */}
+
+
+
+        {/* <div className="" role="alert"> ดูรายละเอียด</div> */}
+
+        <div className="d-grid gap-2">
+          <button className="btn btn-info" type="button">สนใจแผนประกัน</button>
+        </div>
 
       </div>
     </div>
--- src/app/page copy.js
+++ src/app/page copy.js
@@ -7,13 +7,13 @@ export default function Home() {
     <div className="container-fluid">
 
     <div className="row mt-3">
-      <div className="col-sm-12 col-md-4  mb-3">
+      {/* <div className="col-sm-12 col-md-4  mb-3">
       <Card title="ไทยวิวัฒน์ประกันภัย" description="ประกันรถยนต์ชั้น 1" price="7,500" imageUrl="https://ssw-uat-5guqcv.s3.ap-southeast-1.amazonaws.com/thaivivat_logo_ca2d0f91c3.webp" />
-      </div>
-      <div className="col-sm-12 col-md-4  mb-3">
+      </div> */}
+      <div className="col-sm-12 col-md-5  mb-6">
       <Card title="ทิพยประกันภัย" description="ประกันรถยนต์ชั้น 1" price="5,999" imageUrl="https://ssw-uat-5guqcv.s3.ap-southeast-1.amazonaws.com/tip_logo_cca7bc8737.webp" />
       </div>
-      <div className="col-sm-12 col-md-4  mb-3">
+      <div className="col-sm-12 col-md-5  mb-6">
       <Card title="เมืองไทยประกันภัย" description="ประกันรถยนต์ชั้น 1" price="6,300" imageUrl="https://ssw-uat-5guqcv.s3.ap-southeast-1.amazonaws.com/muangth_logo_5e049e5f2c.webp" />
       </div>
     </div>
